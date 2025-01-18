f7e72a4c58c9e8b2ceb12aa9ce6ae816f5a4668a
asideofcode-dev <kumbirai@asideofcode.com>
2025-01-18 20:07:26 +0000

    Add constrast to title

--- src/app/((search))/search.page.tsx
+++ src/app/((search))/search.page.tsx
@@ -42,7 +42,7 @@ export default function SearchPageWrapper() {
     <main className="max-w-3xl mx-auto px-4 sm:px-0">
       <Appbar />
       <div className="mb-6">
-        <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
+        <h1 className="text-4xl font-bold text-blue-600 dark:text-blue-500">
           Duramazwi
         </h1>
         <p className="text-lg text-gray-600 dark:text-gray-400">
