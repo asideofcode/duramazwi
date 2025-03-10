59d0961f2c1065686a9810c058d54cf685e01bdd
ftanekha <farai.tanekha@gmail.com>
2025-03-10 23:46:10 +0000

    - set the type for the object containing metadata for each word

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,17 @@
 import { MetadataRoute } from 'next';
 import data from '@/data/data.json';
 
+type wordObject = {//set the expected types for each property value
+  url: string
+  lastModified: string,
+  changeFrequency: "daily" | "always" | "hourly" | "weekly" | "monthly" | "yearly" | "never" | undefined
+  priority: number,
+}
+
+type wordObjectArray = wordObject[] //an array of objects containing information about each word
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const words = data.map((word) => ({
+  const words: wordObjectArray = data.map((word) => ({
     url: `https://dictionary.chishona.org/word/${encodeURIComponent(word.word)}`,
     lastModified: new Date().toISOString(),
     changeFrequency: 'daily',
