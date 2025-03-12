a43db04ba7f3ba662b85d49440943e6c7674aac7
ftanekha <farai.tanekha@gmail.com>
2025-03-12 18:51:36 +0000

    - AppBar nav items change apperance to indicate the current/active route - refactor nav items classLists for DRY purposes

--- src/component/appbar.component.tsx
+++ src/component/appbar.component.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import { useTheme } from "@/app/hook/use-theme.hook";
 import SvgIcon from "@/component/icons/svg-icon";
 import Link from "next/link";
+import {usePathname} from 'next/navigation'
 
 const inter = Inter({
   subsets: ["latin"],
@@ -15,23 +16,24 @@ const inter = Inter({
  * Appbar component
  */
 export default function Appbar() {
-  // const [isDark, setIsDark] = React.useState(false);
   const { toggleTheme, darkMode } = useTheme();
-  // const router = useRouter();
+  const pathname = usePathname();
+
+  const navBarItemClassList = 'h-6 w-6 cursor-pointer hover:text-blue-500 hover:scale-110 transition-transform duration-200';
 
   return (
-    <div className={`${inter.className}`}>
+    <div className={`${inter.className} `}>
       <nav className="flex place-content-between py-6 theme-text-h3 ">
         <div>
           <Link href="/">
             <SvgIcon
-              className="h-6 w-6 cursor-pointer hover:text-blue-500 hover:scale-110 transition-transform duration-200"
+              className={`${navBarItemClassList} ${pathname === '/' && 'active'}`}
               icon={"Book"} title="return to homepage"
             />
           </Link>
           <Link href="/suggest">
             <SvgIcon
-              className="h-6 w-6 cursor-pointer hover:text-blue-500 hover:scale-110 transition-transform duration-200"
+              className={`${navBarItemClassList} ${pathname === '/suggest' && 'active'}`}
               icon={"Plus"} title="suggest a new word"
             />
           </Link>
@@ -46,7 +48,7 @@ export default function Appbar() {
           >
             <div className="flex items-center">
               <SvgIcon
-                className="h-6 w-6 cursor-pointer hover:text-blue-500 hover:scale-110 transition-transform duration-200"
+                className={navBarItemClassList}
                 variant={!darkMode ? "dark" : "light"}
                 icon={"LightDark"}
               />
